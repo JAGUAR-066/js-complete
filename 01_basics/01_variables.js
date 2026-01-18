181e0104bc11597d000037afdbfca5cb79415982
JAGDISH SAHU <righteousknight007@gmail.com>
2026-01-18 15:45:01 +0000

    learned variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const account_Id = 0125245
+let account_email = "jaguar345@hotmail.com"
+var account_password = "12345"
+account_city = "jaipur"
+let account_state;
+
+// don't use var only use const and let
+
+account_email = "james127@jagmail.com"
+account_password = "2353643"
+account_city="delhi"
+
+console.table([account_Id,account_email,account_password,account_city,account_state])
